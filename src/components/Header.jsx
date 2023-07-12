65f40ade7de7dd25d0a32dff726c08d411b57fb1
Francisco Villalba Garcia <franvillalba@me.com>
2023-07-12 16:21:31 +0200

    dia 3-2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import IconMoon from "./icons/IconMoon";
 import IconSun from "./icons/IconSun";
 
+const initialDarkMode = document.documentElement.className.includes("dark");
+
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(initialDarkMode);
+
+    // Este código se ejecuta cada vez que cambia el darkMode
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.theme = "dark";
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        }
+    }, [darkMode]);
+
     return (
         <header className="container mx-auto px-4 pt-8">
             <div className="flex justify-between">
-                <h1 className="uppercase text-white text-3xl font-semibold tracking-[.5em]">Todo</h1>
-                <button><IconMoon fill={'#fff'} /></button>
+                <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
+                    Todo
+                </h1>
+                <button onClick={() => setDarkMode(!darkMode)} title="Pulsa para cambiar fondo">
+                    {darkMode ? <IconSun /> : <IconMoon />}
+                </button>
             </div>
         </header>
     );
-}
+};
 
 export default Header;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,7 +12,7 @@ const TodoItem = ({todo, removeTodo, updateTodo}) => {
                 }`}>
                 { completed && <IconCheck /> }
             </button>
-            <p className={`text-gray-600 grow ${completed && "line-through"}`}>{title}</p>
+            <p className={`text-gray-600 dark:text-gray-400 transition-all duration-1000 grow ${completed && "line-through"}`}>{title}</p>
             <button onClick={() => (removeTodo(id))} className="flex-none"><IconCross /></button>
         </article>
     );
